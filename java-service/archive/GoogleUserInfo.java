966ccd13239defe7babebe3b0532e87ee0c79105
Martina Burjanova <martina.burjanova@konicaminolta.cz>
2020-11-06 08:14:13 +0100

    removed archive from .gitignore files

---
+++ java-service/archive/GoogleUserInfo.java
@@ -0,0 +1,31 @@
+package da.project.sporteezone.app.entity;
+
+import java.util.Map;
+
+/*
+public class GoogleUserInfo {
+
+    private Map<String, Object> attributes;
+
+    public GoogleUserInfo(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    public String getId() {
+        return (String) attributes.get("sub");
+    }
+
+    public String getName() {
+        return (String) attributes.get("name");
+    }
+
+    public String getEmail() {
+        return (String) attributes.get("email");
+    }
+
+    public String getImageUrl() {
+        return (String) attributes.get("picture");
+    }
+}
+
+*/
