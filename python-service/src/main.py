e40055adf1be8d1c366a6a6698130f4ca1d33e40
Pajinka <62245699+Pajinka@users.noreply.github.com>
2020-11-22 16:18:46 +0100

    Update main.py
    pridany ty dve fitka

--- python-service/src/main.py
+++ python-service/src/main.py
@@ -4,13 +4,20 @@ import logging
 import irongym
 import bigonefitness_rev
 import send_data_to_api
+import kamenak
+import velky_pruvan
 
 def execute():
     vysledek_irongym = irongym.scraping()
     vysledek_bigonefitness = bigonefitness_rev.scraping_obf()
+    vysledek_kamenak = kamenak.kamenak()
+    vysledek_velky_pruvan = velky_pruvan.velky_pruvan()
     logging.info(vysledek_irongym)
+    
     send_data_to_api.send_data(vysledek_irongym)
     send_data_to_api.send_data(vysledek_bigonefitness)
+    send_data_to_api.send_data(vysledek_kamenak)
+    send_data_to_api.send_data(vysledek_velky_pruvan)
 
 
 if __name__=="__main__":
