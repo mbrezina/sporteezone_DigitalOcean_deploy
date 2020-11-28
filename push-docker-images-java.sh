fe57d398c81ef6bdb31455e8116fe779d779c8d1
Martina Burjanova <martina.burjanova@konicaminolta.cz>
2020-11-28 10:53:59 +0100

    image built but does not run on DigitalOcean

--- push-docker-images-java.sh
+++ push-docker-images-java.sh
@@ -2,10 +2,4 @@
 
 docker tag da-sporteezone-java:1.0.4 martiik/da-sporteezone-java:1.0.4
 
-#docker tag da-projekt-do-deploy-java:1.0.0 dolejs/da-projekt-do-deploy-java:1.0.0
-##docker tag da-projekt-do-deploy-python:1.0.0 dolejs/da-projekt-do-deploy-python:1.0.0
-
-###docker push martiik/da-sporteezone-java:tagname
-
 docker push martiik/da-sporteezone-java:1.0.4
-###docker push dolejs/da-projekt-do-deploy-python:1.0.0
--- push-docker-images-js.sh
+++ push-docker-images-js.sh
@@ -2,10 +2,4 @@
 
 docker tag da-sporteezone-js:1.0.1 martiik/da-sporteezone-js:1.0.1
 
-#docker tag da-projekt-do-deploy-java:1.0.0 dolejs/da-projekt-do-deploy-java:1.0.0
-##docker tag da-projekt-do-deploy-python:1.0.0 dolejs/da-projekt-do-deploy-python:1.0.0
-
-###docker push martiik/da-sporteezone-java:tagname
-
 docker push martiik/da-sporteezone-js:1.0.1
-###docker push dolejs/da-projekt-do-deploy-python:1.0.0
